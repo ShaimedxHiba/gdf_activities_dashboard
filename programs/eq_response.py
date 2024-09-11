e24fec93cd2b59ef1529ffb47fa70723864017a1
ShaimedxHiba <159702816+ShaimedxHiba@users.noreply.github.com>
2024-09-11 13:15:48 +0100

    Create eq_response.py

---
+++ programs/eq_response.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def eq_response():
+    st.header("EQ Response Program")
+    st.write("This page contains data and visualizations for the EQ Response program.")
+
+if __name__ == "__main__":
+    eq_response()
