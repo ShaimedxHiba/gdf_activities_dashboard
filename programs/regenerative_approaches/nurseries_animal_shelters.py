cb9acbedc3cd36e2f1eb0215b576aa3e9f0f8457
ShaimedxHiba <159702816+ShaimedxHiba@users.noreply.github.com>
2024-09-11 13:17:32 +0100

    Create nurseries_animal_shelters.py

---
+++ programs/regenerative_approaches/nurseries_animal_shelters.py
@@ -0,0 +1,7 @@
+
+
+import streamlit as st
+
+def nurseries_animal_shelters():
+    st.header("Nurseries and animal shelters")
+    st.write("This page contains data and visualizations for the nurseries and animal shelters sub-program.")
