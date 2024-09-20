a91ac08204b81eb8dbde2bcb96776cc0e75bda3d
ShaimedxHiba <159702816+ShaimedxHiba@users.noreply.github.com>
2024-09-20 15:35:32 +0100

    Delete programs/eq_response.py

--- programs/eq_response.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def eq_response():
-    st.header("EQ Response Program")
-    st.write("This page contains data and visualizations for the EQ Response program.")
-
-if __name__ == "__main__":
-    eq_response()
