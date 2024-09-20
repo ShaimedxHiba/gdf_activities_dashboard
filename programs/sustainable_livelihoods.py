780f21c417b3100b1fb9f1a019fd7cb89fca6e2e
ShaimedxHiba <159702816+ShaimedxHiba@users.noreply.github.com>
2024-09-20 15:35:50 +0100

    Delete programs/sustainable_livelihoods.py

--- programs/sustainable_livelihoods.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def sustainable_livelihoods():
-    st.header("Sustainable Livelihoods")
-    st.write("This page contains data and visualizations for the sustainable livelihoods program.")
-
-if __name__ == "__main__":
-    sustainable_livelihoods()
