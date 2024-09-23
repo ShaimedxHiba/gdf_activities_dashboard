3a2166b8d2031792b8811b1363388568b66a6148
ShaimedxHiba <159702816+ShaimedxHiba@users.noreply.github.com>
2024-09-23 10:03:34 +0100

    Delete programs/sustainable_livelihoods directory

--- programs/sustainable_livelihoods/action_coop.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import numpy as np  # for generating random progress values
-
-def action_coop():
-    st.title('test')
--- programs/sustainable_livelihoods/cooperatives.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import numpy as np  # for generating random progress values
-
-def cooperatives():
-     st.title('test')
