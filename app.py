b6534073e3f5efb2db40ab4d8bcb2aebae9447b3
ShaimedxHiba <159702816+ShaimedxHiba@users.noreply.github.com>
2024-10-06 17:23:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,4 @@ from programs.chp import chp
 from programs.pp import pp
 
 
-# Sidebar navigation
-st.sidebar.title("Cultural Heritage Preservation")
-program_selection = st.sidebar.radio("Select Program", [
-    "Overview",
-    "Pilot Project"
-])
-
-# Conditional page loading
-if program_selection == "Overview":
-    chp()
-
-elif program_selection == "Pilot Project":
-    pp()
+chp()
