40f0fb1545490c79b836be4d53fadebf886d012a
ShaimedxHiba <159702816+ShaimedxHiba@users.noreply.github.com>
2024-10-24 18:07:32 +0100

    Update animal_shelters.py

--- programs/regenerative_approaches/animal_shelters.py
+++ programs/regenerative_approaches/animal_shelters.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import folium
+from streamlit_folium import folium_static
+
+
 
 
 def animal_shelters():
-     st.title('Animal Shelters')
+     st.title('Livestock & Shelters')
      
